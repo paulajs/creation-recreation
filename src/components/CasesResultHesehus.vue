8edefbc37108bfcf6d6732a8c3a93803ae439c27
Paula <paulajoannasobczak@gmail.com>
2019-10-07 03:39:10 +0200

    several new components ans pages

---
+++ src/components/CasesResultHesehus.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="cases-result-hesehus">
+    <ButtonVisitPage />
+    <div class="display-results">
+      <h2 class="result-header">Videos</h2>
+      <VideoCaseComponent
+        videoSection="video-section section-matas"
+        videoName="Matas Video"
+        :videoSrc="getMatasVideo()"
+        :videoPoster="getMatasPoster()"
+      />
+      <VideoCaseComponent
+        videoSection="video-section section-fristads"
+        videoName="Fristads Kansas Video"
+        :videoSrc="getFristadsVideo()"
+        :videoPoster="getFristadsPoster()"
+      />
+      <VideoCaseComponent
+        videoSection="video-section section-inspiration"
+        videoName="Inspiration Video"
+        :videoSrc="getInspirationVideo()"
+        :videoPoster="getInspirationPoster()"
+      />
+      <VideoCaseComponent
+        videoSection="video-section section-danoffice"
+        videoName="Danoffice Video"
+        :videoSrc="getDanofficeVideo()"
+        :videoPoster="getDanofficePoster()"
+      />
+      <VideoCaseComponent
+        videoSection="video-section section-kunderejse"
+        videoName="Customer journey Video"
+        :videoSrc="getJourneyVideo()"
+        :videoPoster="getJourneyPoster()"
+      />
+    </div>
+  </div>
+</template>
+<style lang="scss">
+@import "../assets/sass/_global.scss";
+
+$transition: all 0.1s ease-in;
+
+.cases-result-hesehus {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .display-results {
+    display: grid;
+    grid-template-rows: repeat(110, 4.94vh);
+    grid-row-gap: 1vh;
+    grid-template-columns: repeat(24, 3.125vw);
+    grid-column-gap: 1vw;
+    padding: 2vh 0.55vw;
+    background: white;
+    h2.result-header {
+      grid-column-start: 2;
+      grid-column-end: 24;
+      grid-row-start: 1;
+      grid-row-end: 4;
+      font-family: $font-primary;
+      font-size: 87px;
+      color: white;
+      transform: translateY(-184px) translateX(-17.068px);
+      font-size: 150px;
+      color: black;
+    }
+  }
+}
+</style>
+<script>
+import VideoCaseComponent from "@/components/VideoCaseComponent.vue";
+import ButtonVisitPage from "@/components/ButtonVisitPage.vue";
+
+const matasVideo = require("../assets/videos/hesehus/Matas1.mp4");
+const matasPoster = require("../assets/videos/hesehus/Matas1.png");
+const fristadsVideo = require("../assets/videos/hesehus/Fristads-kansas1.mp4");
+const fristadsPoster = require("../assets/videos/hesehus/Fristads-kansas1.png");
+const inspirationVideo = require("../assets/videos/hesehus/Inspiration1.mp4");
+const inspirationPoster = require("../assets/videos/hesehus/Inspiration1.png");
+const danofficeVideo = require("../assets/videos/hesehus/Danoffice1.mp4");
+const danofficePoster = require("../assets/videos/hesehus/Danoffice1.png");
+const journeyVideo = require("../assets/videos/hesehus/Kunderejse1.mp4");
+const journeyPoster = require("../assets/videos/hesehus/Kunderejse1.png");
+export default {
+  name: "CasesResultHesehus",
+  methods: {
+    getMatasVideo: () => matasVideo,
+    getMatasPoster: () => matasPoster,
+    getFristadsVideo: () => fristadsVideo,
+    getFristadsPoster: () => fristadsPoster,
+    getInspirationVideo: () => inspirationVideo,
+    getInspirationPoster: () => inspirationPoster,
+    getDanofficeVideo: () => danofficeVideo,
+    getDanofficePoster: () => danofficePoster,
+    getJourneyVideo: () => journeyVideo,
+    getJourneyPoster: () => journeyPoster
+  },
+  components: {
+    VideoCaseComponent,
+    ButtonVisitPage
+  }
+};
+</script>
---
+++ src/components/CasesResultNmg.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="cases-result-nmg">
+    <div class="display-results">
+      <div class="nmg-design-obj">
+        <h2>Design objectives</h2>
+        <div class="postits">
+          <img src="../assets/img/SVG/postit1.svg" alt />
+          <img src="../assets/img/SVG/postit2.svg" alt />
+          <img src="../assets/img/SVG/postit3.svg" alt />
+          <img src="../assets/img/SVG/postit4.svg" alt />
+        </div>
+      </div>
+      <h2 class="result-header">3D models</h2>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+@import "../assets/sass/_global.scss";
+
+$transition: all 0.1s ease-in;
+
+.cases-result-nmg {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .display-results {
+    display: grid;
+    grid-template-rows: repeat(110, 4.94vh);
+    grid-row-gap: 1vh;
+    grid-template-columns: repeat(24, 3.125vw);
+    grid-column-gap: 1vw;
+    padding: 2vh 0.55vw;
+    background: white;
+    .nmg-design-obj {
+      grid-column-start: 1;
+      grid-column-end: 25;
+      grid-row-start: 1;
+      grid-row-end: 11;
+      background: linear-gradient(#ff90ff -29%, #2f97b3 152%);
+      display: grid;
+    grid-template-rows: repeat(30, 4.94vh);
+    grid-row-gap: 1vh;
+    grid-template-columns: repeat(24, 3.125vw);
+    grid-column-gap: 1vw;
+        border: 1px solid black;
+      h2{
+        grid-column-start: 2;
+        grid-column-end: 24;
+        font-family: $font-primary;
+        color: white;
+        font-size: 87px
+      }
+      .postits {
+        grid-column-start: 2;
+    grid-column-end: 24;
+    grid-row-start: 4;
+    grid-row-end: 10;
+    display: flex;
+    flex-direction: row;
+        img{
+          width: 25%;
+          height: 38vh;
+        }
+      }
+    }
+    h2.result-header {
+      grid-column-start: 2;
+      grid-column-end: 24;
+      grid-row-start: 14;
+      grid-row-end: 16;
+      font-family: $font-primary;
+      color: white;
+      font-size: 94px;
+      color: rgb(27, 25, 25);
+    }
+  }
+}
+</style>
